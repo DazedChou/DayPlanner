5243203f8c41a172c9291c98a02954ec081def86
David Chou <davidchou2009@gmail.com>
2022-01-04 18:03:50 -0800

    add display date

---
+++ script.js
@@ -0,0 +1,8 @@
+var today = $("#currentDay");
+var planner = $(".container-fluid");
+var hours = ["9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00",
+"19:00","20:00",];
+
+today.text(moment().format('MMMM Do YYYY'));
+
+
