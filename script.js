d12b9200fe4d3247b6a27dbeb51d0dcf6115975c
David Chou <davidchou2009@gmail.com>
2022-01-04 18:04:43 -0800

    display time blocks when page loads

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ var hours = ["9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:
 today.text(moment().format('MMMM Do YYYY'));
 
 
+for (let i = 0 ; i < hours.length ; i++){
+    var rowEl = $('<div class = "row">');
+    planner.append(rowEl);
+
+    timeEl = $('<div class ="col-md-1 time-block">');
+    timeEl.text(hours[i]);
+    rowEl.append(timeEl);
+
+    noteEl = $('<div class = "col-md-10 description">');
+    textEl = $('<textarea>');
+    noteEl.append(textEl);
+
+    rowEl.append(noteEl);
+
+    saveEl = $('<div class = "col-md-1">')
+    buttonEl = $('<button class = "saveBtn">')
+    saveEl.append(buttonEl);
+    buttonEl.text("Save");
+    rowEl.append(saveEl);
+}
